c8ed8b014368ab3d37ce9d1a7c1da4acf3754810
Walecole <Walecole>
2020-06-21 19:25:15 +0100

    Plot graph; export to file

--- die_visuals.py
+++ die_visuals.py
@@ -1,10 +1,25 @@
 from die import Die
+import pygal
 
 die = Die()
 
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = die.roll_die()
     results.append(result)
 
-print(results)
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+#Visualiza results.
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Results"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
