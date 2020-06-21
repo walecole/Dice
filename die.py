5bcef097ce8bf67993e81f5d00d7b6ec1f19a7bd
Walecole <Walecole>
2020-06-21 16:04:40 +0100

    Add die visuals

--- die.py
+++ die.py
@@ -1,1 +1,10 @@
 from random import randint
+
+class Die():
+    #A class representing a single die
+
+    def __init__(self, num_sides = 6):
+        self.num_sides = num_sides
+
+    def roll_die(self):
+        return randint(1, self.num_sides)
